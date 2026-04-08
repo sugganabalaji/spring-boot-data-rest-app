3ec5167860e50d0dee9c160e22a648cadab2a43b
Balaji, Suggana <sugganabalaji@gmail.com>
2026-04-09 04:11:12 +0530

    Rest repository can access from postman

---
+++ src/main/java/com/app/model/JobPost.java
@@ -0,0 +1,29 @@
+package com.app.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Component
+@Entity
+public class JobPost {
+
+	@Id
+	private int postId;
+	@Column(length = 50)
+	private String postProfile;
+	private String postDesc;
+	private int reqExperience;
+	private List<String> postTechStack;
+	
+
+}
