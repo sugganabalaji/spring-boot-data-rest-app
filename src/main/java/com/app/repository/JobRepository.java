4d92075039c2ce048dd7603c30488c2833306581
Balaji, Suggana <sugganabalaji@gmail.com>
2026-04-09 04:46:45 +0530

    @RepositoryRestResource annotation

--- src/main/java/com/app/repository/JobRepository.java
+++ src/main/java/com/app/repository/JobRepository.java
@@ -4,6 +4,6 @@ import com.app.model.JobPost;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
+@Repository//RestResource(collectionResourceRel = "jobPosts", path = "jobPosts")
 public interface JobRepository extends JpaRepository<JobPost, Integer> {
 }
